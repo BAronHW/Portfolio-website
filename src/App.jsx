b9103e8d391d761c717ee9afa661b4f3183c1140
unknown <aaronwan808@gmail.com>
2024-07-23 20:27:36 +0100

    added more projects

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import genetic from './assets/images/genetic1.png'
 import library from './assets/images/Library.png'
 import port from './assets/images/port.png'
 import network from './assets/images/network.png'
+import TYP from './assets/images/TYP.png'
+import ecom from './assets/images/ecomm.png'
 import './App.css'
 
 const App = () => {
@@ -49,6 +51,8 @@ const App = () => {
           <Projects projectImg={library} projectTitle={"E-commerce Website"} projectDesc={"Created an E-commerce website using React.js while fetching data from an API and dynamically displaying the data on the front-end"}></Projects>
           <Projects projectImg={port} projectTitle={"Portfolio Website"} projectDesc={"Designed and created this portfolio website using a tech-stack of react.js and css to demonstrate skills with react and proficiency at utilizing moern technologies to create an engaging end product"}></Projects>
           <Projects projectImg={network} projectTitle={"Re-creatng Ping and Traceroute"} projectDesc={"Completed a project focused on socket programming, utilizing the socket module to recreate classic networking tools such as Ping and Traceroute"}></Projects>
+          <Projects projectImg={ecom} projectTitle={"CRUD Notes App"} projectDesc={"Created a simple fullstack CRUD app using the MERN stack(MongoDB, Express.js, React.js and Node.js). The purpose of this project was to develop a greater understanding of REST API's"}></Projects>
+          <Projects projectImg={TYP} projectTitle={"Automatic grading from mammogram images"} projectDesc={"My Bachelor's thesis explored automated breast cancer diagnosis using machine learning and computer vision, employing methods like FRCNN and VGG. This project deepened my interest in AI and familiarized me with Python libraries like NumPy and Pandas for results analysis."}></Projects>
         </div>
       </div>
       <div className='contact-container'>
